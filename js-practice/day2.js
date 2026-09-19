73c40a3b29706ded4b2b19ca13f4dd33e3ab3f73
Theophilus-git <theophilusofori819@gmail.com>
2026-09-19 10:54:22 +0000

    object in javascript

---
+++ js-practice/day2.js
@@ -0,0 +1,35 @@
+// I am learning in the terminal
+//  So far I have studied how to work with string and numbers
+// How to declare a variable and how to use the Math  functions.let
+
+// An object in Javascript
+
+let contact = {
+ firstName:"Theophilus",
+ lastName:"Ofori",
+ email:"theophilusofori819@gmail.com",
+ phoneNumber:"0547023137",
+ age:23
+ } 
+
+ console.log(contact)
+
+ let bird = {
+    genus:"Corvus",
+    species:"Corvus corax",
+    commonName:"Common Raven",
+    wingspan:1.2,
+    diet:"Omnivorous"
+ }
+
+ console.log(bird)
+
+ let bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote:""
+ }
+
+ console.log(bear)
